1600bd517f1e2799ba558f51a1def9c1899d197d
Alex <112102138+34ykri@users.noreply.github.com>
2024-01-11 14:40:34 -0600

    First Commit

---
+++ wdv341.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>WDV341 Intro PHP</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+	<header>
+		<h1 id="#top">Alex Vang</h1>
+		<h1>WDV341 Intro PHP</h1>
+	</header>
+	<nav>
+			<h2><a href="index.html">Back to Homepage</a></h2>
+		</nav>
+	<footer>
+		<p class="center"><a href="#top">Back to Top</a></p>
+	</footer>
+</body>
+
+</html>
