24a6f2af07e383be05c8effeb16c938382c20a92
Alex <112102138+34ykri@users.noreply.github.com>
2024-03-05 18:16:57 -0600

    Update formHandler.php
    wrap form data display in an if statement so if someone tries to go to the page without any information they get a message.
    
    fix comments so if no comments are added it says no comments provided.
    
    include date in the emails that are sent.

--- Homework/contactEmailProject/formHandler.php
+++ Homework/contactEmailProject/formHandler.php
@@ -24,114 +24,124 @@ body {
 	$comments = $_POST["comments"];
 	$date = date("m/d/Y");
 	echo "<div class='bdr2'>";
-	echo "<h1>Dear $contact_name,</h1>";
-	echo "<p>Thank you for contacting us!</p>";
-	
-	echo "<p>We have noted down your email: $contact_email</p>";
+	// This if statement is used so if a user tries to access the formHandler.php page
+	// without any information provided, wed can give them an appropriate response.
+	if($contact_name != "" && $contact_email){
+		echo "<h1>Dear $contact_name,</h1>";
+		echo "<p>Thank you for contacting us!</p>";
+		
+		echo "<p>We have noted down your email: $contact_email</p>";
 
-	echo "<p>We will contact you with more information regarding:</p>";
+		echo "<p>We will contact you with more information regarding:</p>";
 
-	echo "<p>Reasons for Contact:</p>";
-	echo "<p class='margin_style1'>- $reasons</p>";
+		echo "<p>Reasons for Contact:</p>";
+		echo "<p class='margin_style1'>- $reasons</p>";
 
-	echo "<p>You have shared the following comments which we will review:</p>";
-	if($comments == "") {
-		echo "<p class='margin_style1'>No comments provided.</p>";
-	}
-	else {
-		echo "<p class='margin_style1'>- $comments</p>";
-	}
-	echo "<p>Date of Contact: $date</p>";
-
-	// Email user and person being Contacted
-	$to = $contact_email;
-	$subject = "Hello $contact_name, Thank you for your inquiry.";
-	$message = "<html>
-				<head>
-					<style>
-						a:link{
-							color: #39ff14;
-						}
-						a:hover {
-							color: #289bff;
-						}
-						a:active {
-							color:#71ff4a;
-						}
-						
-					</style>
-				</head>
-                <body style='font-family: Arial, Helvetica, sans-serif;'>
-					<br>
-					<div style='background-color: black; color: #81ab92; width: 90%; text-align: left; border: 2px solid #81ab92; padding: 10px; margin: 20px; margin-left: auto; margin-right: auto; border-radius: 5px; overflow: auto;'>
-						<h1>Dear $contact_name,</h1>
-						<p>We have received your email and will respond regarding the information you have given us.</p>
-						<p>We will contact you at this email: $contact_email</p>
-						<p>Your reason for contact:</p>
-							<p style='margin-left: 20px;'>- $reasons</p>
-						<p>Comments:</p>
-							<p style='margin-left: 20px;'>- $comments</p>
-						<p>Thank you for contacting us!</p>
-						<p>We will be back with you shortly.</p>
-						<p style='text-align:center;'>wdv341.chaeonline.net</p>
-					</div>
-					<br>
-                </body>
-            </html>";
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	$headers .= "From: info@wdv341.chaeonline.net" . "\r\n"; 
-	
-	// Make sure email entered is valid, if valid we can send the confirmation email
-	if (filter_var($to, FILTER_VALIDATE_EMAIL)) {
-		$send_mail = mail($to, $subject, $message, $headers);
-		if ($send_mail) {
-			echo "<h3 style='color: #39ff14; text-align: center;'>Email sent successfully!</h3>";
-			// Send to Staff email if the email was successfully sent to the client
-			$to = "staff@wdv341.chaeonline.net";
-			$subject = "A client: $contact_email has submitted a form to contact us.";
-			$message = "<html>
-						<head>
-							<style>
-								a:link{
-									color: #39ff14;
-								}
-								a:hover {
-									color: #289bff;
-								}
-								a:active {
-									color:#71ff4a;
-								}
-								
-							</style>
-						</head>
-						<body style='font-family: Arial, Helvetica, sans-serif;'>
-							<br>
-							<div style='background-color: black; color: #81ab92; width: 90%; text-align: left; border: 2px solid #81ab92; padding: 10px; margin: 20px; margin-left: auto; margin-right: auto; border-radius: 5px; overflow: auto;'>
-								<p>Client Name: $contact_name,</p>
-								<p>Client Email: $contact_email</p>
-								<p>Reason for Contact:</p>
-									<p style='margin-left: 20px;'>- $reasons</p>
-								<p>Comments:</p>
-									<p style='margin-left: 20px;'>- $comments</p>
-								<p>Please respond to client as soon as possible.</p>
-							</div>
-							<br>
-						</body>
-					</html>";
-			$headers = "MIME-Version: 1.0" . "\r\n";
-			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-			$headers .= "From: info@wdv341.chaeonline.net" . "\r\n"; 
-			$send_mail = mail($to, $subject, $message, $headers);
-		} 
+		echo "<p>You have shared the following comments which we will review:</p>";
+		if($comments == "") {
+			$comments = "No comments provided.";
+			echo "<p class='margin_style1'>- $comments</p>";
+		}
 		else {
-			echo "<h3 style='color: red; text-align: center;'>Failed to send email. Please try again.</h3>";
+			echo "<p class='margin_style1'>- $comments</p>";
+		}
+		echo "<p>Date of Contact: $date</p>";
+	
+		// Email user and person being Contacted
+		$to = $contact_email;
+		$subject = "Hello $contact_name, Thank you for your inquiry.";
+		$message = "<html>
+					<head>
+						<style>
+							a:link{
+								color: #39ff14;
+							}
+							a:hover {
+								color: #289bff;
+							}
+							a:active {
+								color:#71ff4a;
+							}
+							
+						</style>
+					</head>
+					<body style='font-family: Arial, Helvetica, sans-serif;'>
+						<br>
+						<div style='background-color: black; color: #81ab92; width: 90%; text-align: left; border: 2px solid #81ab92; padding: 10px; margin: 20px; margin-left: auto; margin-right: auto; border-radius: 5px; overflow: auto;'>
+							<h1>Dear $contact_name,</h1>
+							<p>We have received your email and will respond regarding the information you have given us.</p>
+							<p>We will contact you at this email: $contact_email</p>
+							<p>Your reason for contact:</p>
+								<p style='margin-left: 20px;'>- $reasons</p>
+							<p>Comments:</p>
+								<p style='margin-left: 20px;'>- $comments</p>
+							<p>Date of Contact: $date</p>
+							<p>Thank you for contacting us!</p>
+							<p>We will be back with you shortly.</p>
+							<p style='text-align:center;'>wdv341.chaeonline.net</p>
+						</div>
+						<br>
+					</body>
+				</html>";
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers .= "From: info@wdv341.chaeonline.net" . "\r\n"; 
+		
+		// Make sure email entered is valid, if valid we can send the confirmation email
+		if (filter_var($to, FILTER_VALIDATE_EMAIL)) {
+			$send_mail = mail($to, $subject, $message, $headers);
+			if ($send_mail) {
+				echo "<h3 style='color: #39ff14; text-align: center;'>Email sent successfully!</h3>";
+				// Send to Staff email if the email was successfully sent to the client
+				$to = "staff@wdv341.chaeonline.net";
+				$subject = "A client: $contact_email has submitted a form to contact us.";
+				$message = "<html>
+							<head>
+								<style>
+									a:link{
+										color: #39ff14;
+									}
+									a:hover {
+										color: #289bff;
+									}
+									a:active {
+										color:#71ff4a;
+									}
+									
+								</style>
+							</head>
+							<body style='font-family: Arial, Helvetica, sans-serif;'>
+								<br>
+								<div style='background-color: black; color: #81ab92; width: 90%; text-align: left; border: 2px solid #81ab92; padding: 10px; margin: 20px; margin-left: auto; margin-right: auto; border-radius: 5px; overflow: auto;'>
+									<p>Client Name: $contact_name</p>
+									<p>Client Email: $contact_email</p>
+									<p>Reason for Contact:</p>
+										<p style='margin-left: 20px;'>- $reasons</p>
+									<p>Comments:</p>
+										<p style='margin-left: 20px;'>- $comments</p>
+									<p>Date of Contact: $date</p>
+									<p>Please respond to client as soon as possible.</p>
+								</div>
+								<br>
+							</body>
+						</html>";
+				$headers = "MIME-Version: 1.0" . "\r\n";
+				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+				$headers .= "From: info@wdv341.chaeonline.net" . "\r\n"; 
+				$send_mail = mail($to, $subject, $message, $headers);
+			} 
+			else {
+				echo "<h3 style='color: red; text-align: center;'>Failed to send email. Please try again.</h3>";
+			}
+		}
+		else {
+			echo "<h3 style='color: red; text-align: center;'>Could not send email because email address was invalid. Please enter a valid one.</h3>";
 		}
 	}
-	else {
-		echo "<h3 style='color: red; text-align: center;'>Could not send email because email address was invalid. Please enter a valid one.</h3>";
+	else{
+		echo "<h1>Sorry, not enough information was provided to send an email.</h1>";
+		echo "<p>Thank you, please try again.</p>";
 	}
-
 	
 	// echo $contact_name;
 	// echo "<br>";
